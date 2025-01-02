76ab3195df6c2e86de6ec09d61a47f3551d5cd4e
Vitalii Fedyk <vfedyk@softserveinc.com>
2025-01-02 18:30:45 +0200

    Temporary

--- src/main/java/org/vitalii/fedyk/librarybookservice/LibraryBookServiceApplication.java
+++ src/main/java/org/vitalii/fedyk/librarybookservice/LibraryBookServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LibraryBookServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(LibraryBookServiceApplication.class, args);
     }
-
 }
